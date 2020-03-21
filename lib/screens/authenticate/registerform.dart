b22b23f9e10165600df2f980470003071ffec9c2
Ammali Mouna <im_ammali@esi.dz>
2020-03-21 19:33:59 +0100

    Update registerform.dart

--- lib/screens/authenticate/registerform.dart
+++ lib/screens/authenticate/registerform.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/services/auth.dart';
+import 'package:myapp/screens/authenticate/register.dart';
+
 
 class RegisterForm extends StatefulWidget {
 
@@ -11,6 +13,11 @@ class RegisterForm extends StatefulWidget {
   _RegisterFormState createState() => _RegisterFormState();
 }
 
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
 class _RegisterFormState extends State<RegisterForm> {
 
   final AuthService _auth = AuthService();
@@ -20,121 +27,192 @@ class _RegisterFormState extends State<RegisterForm> {
 
   // text field state
   String nom= '';
+    String email= '';
+
   String prenom = '';
   String utilisateur = '';
   String error ='';
   String phoneNumber;
 String phoneIsoCode;
-
-void onPhoneNumberChange(String number, String internationalizedPhoneNumber, String isoCode) {
+int _currentStep=0; 
+/*void onPhoneNumberChange(String number, String internationalizedPhoneNumber, String isoCode) {
     setState(() {
        phoneNumber = number;
        phoneIsoCode = isoCode;
     });
-} 
+} */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
     
       
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        
-        child: Form(
-          key : _formKey, //associer formkey à form
-        child: SingleChildScrollView(
-          
-          child: Column(
-            children: <Widget>[
-             SizedBox(height: 130),
-         ButtonBar(
-            alignment:MainAxisAlignment.center,
+     body: Center(
+       child: Form(
+          key: _formKey,
+          child: Stepper(
+       type: StepperType.horizontal,
+         steps: _mySteps(),
+         currentStep: this._currentStep,
+         onStepTapped: (step){
+           setState(() {
+             this._currentStep= step; 
+           });
+         },
+         onStepContinue: (){
+           setState(() {
+             if(_formKey.currentState.validate() && this._currentStep>=0)
+              this._currentStep=this._currentStep+1; 
+              else{
+                  setState(()=> error = 'Oups il faut tout remplir ! ' );
 
-          children: <Widget>[
-            
-          Material(
-             elevation: 4,
-              borderRadius: BorderRadius.circular(30.0),
-             child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: SizedBox(
-                    height: 53,
-                    width: 53,
-                    child: RaisedButton(
-                        child: Text('1', 
-                        style: const TextStyle(
-                     color:  const Color(0xff26a69a),
-                      fontWeight: FontWeight.w500,
+              }
+           });
+         },
+         onStepCancel: (){
+           setState(() {
+              if(this._currentStep >0 )
+             this._currentStep=this._currentStep-1; 
+             else
+               this._currentStep=0; 
+           });
+          
+         },
+      ), ), 
+       
+      ),
+        
+         
+    );
+  }
+  List<Step> _mySteps(){
+    List<Step> _steps= [
+      Step(
+        title: Text('Etape 1 '),
+        content:Column(children: <Widget>[
+          /*Champs Nom*/ 
+              Material(
+                elevation: 4,
+                 borderRadius: BorderRadius.circular(30.0),
+                child: 
+                TextFormField(
+                    obscureText: false,
+                    //TEXT 
+                   style: TextStyle(
+                      color:  Colors.grey[900], 
                       fontFamily: "Roboto",
                       fontStyle:  FontStyle.normal,
                       fontSize: 16.0
-                  ),),
-                           
-                            shape: StadiumBorder(side: BorderSide(color:  const Color(0xff26a69a))),
-                            color: Colors.white,
-                                onPressed: () {},
-                            ),
-                        ),
-                    ),
-          ),
-          SizedBox(width: 20,), 
-          Material(
-             elevation: 4,
-              borderRadius: BorderRadius.circular(30.0),
-              color: Colors.red,  
-             child: FittedBox(
-                fit: BoxFit.scaleDown,
-                
-                child: SizedBox(
-                    height: 53,
-                    width: 53,
-                    child: RaisedButton(
-                  
-                        child: Text('2', 
-                        style: const TextStyle(
-                     color:  const Color(0xff26a69a),
-                      fontWeight: FontWeight.w500,
+                    ), 
+                    //SHAPE
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        hintText: "Nom",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  ),
+                  //Validation de l'entrée 
+                  validator: (val) => val.isEmpty ? 'Entrez votre nom' : null,
+                  onChanged: (val) {
+                  setState(() => nom = val);
+                },
+              ),
+              ), 
+              SizedBox(height: 12),
+              /*Champs Prenom*/ 
+              Material(
+                elevation: 4,
+                 borderRadius: BorderRadius.circular(30.0),
+                child: 
+                TextFormField(
+                    obscureText: false,
+                    keyboardType: TextInputType.text,
+                    //TEXT 
+                   style: TextStyle(
+                      color:  Colors.grey[900], 
                       fontFamily: "Roboto",
                       fontStyle:  FontStyle.normal,
                       fontSize: 16.0
-                  ),),                  
-                            shape: StadiumBorder(side: BorderSide(color:  const Color(0xff9e9e9e),)),
-                            color: Colors.white,
-                                onPressed: () {},
-                            ),
-                        ),
-                    ),
-          ),
-          SizedBox(width: 20,), 
-          Material(
-             elevation: 4,
-              borderRadius: BorderRadius.circular(30.0),
-             child: FittedBox(
-                fit: BoxFit.scaleDown,
-                
-                child: SizedBox(
-                    height: 53,
-                    width: 53,
-                    child: RaisedButton(
-                        child: Text('3', 
-                        style: const TextStyle(
-                     color:  const Color(0xff26a69a),
-                      fontWeight: FontWeight.w500,
+                    ), 
+                    //SHAPE
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        hintText: "Prenom",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  ),
+                  //Validation de l'entrée 
+                  validator: (val) => val.isEmpty ? 'Entrez votre prénom' : null,
+                  onChanged: (val) {
+                  setState(() => prenom = val);
+                },
+              ),
+              ), 
+                 SizedBox(height: 12),
+              /*Champs Numtle*/ 
+              Material(
+                elevation: 4,
+                 borderRadius: BorderRadius.circular(30.0),
+                child: 
+                TextFormField(
+                    obscureText: false,
+                   keyboardType: TextInputType.emailAddress,
+
+                    //TEXT 
+                   style: TextStyle(
+                      color:  Colors.grey[900], 
                       fontFamily: "Roboto",
                       fontStyle:  FontStyle.normal,
                       fontSize: 16.0
-                  ),),
-                           
-                            shape: StadiumBorder(side: BorderSide(color:  const Color(0xff9e9e9e),)),
-                            color: Colors.white,
-                                onPressed: () {},
-                            ),
-                        ),
-                    ),
-          ),
-             ], ), 
-             /*Champs nom*/ 
+                    ), 
+                    //SHAPE
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        hintText: "Email",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  ),
+                  //Validation de l'entrée 
+                  validator: (val) => val.isEmpty ? 'Entrez votre email' : null,
+                  onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              ), 
+                 SizedBox(height: 12),
+              /*Champs Numtle*/ 
+              Material(
+                elevation: 4,
+                 borderRadius: BorderRadius.circular(30.0),
+                 
+                child: 
+                TextFormField(
+                    obscureText: false,
+                    keyboardType: TextInputType.phone,
+                    //TEXT 
+                   style: TextStyle(
+                      color:  Colors.grey[900], 
+                      fontFamily: "Roboto",
+                      fontStyle:  FontStyle.normal,
+                      fontSize: 16.0
+                    ), 
+                    //SHAPE
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                        hintText: "Numéro de téléphone",
+                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+                  ),
+                  //Validation de l'entrée 
+                  validator: (val) => val.isEmpty ? 'Entrez votre numéro de téléphone' : null,
+                  onChanged: (val) {
+                  setState(() => phoneNumber = val);
+                },
+              ),
+              ), 
+        ],), 
+        isActive: _currentStep >=0,  ),
+        Step(
+        title: Text('Etape 2'),
+        content:Column(children: <Widget>[
+           /*Champs nom d'utilisateur*/ 
              SizedBox(height: 76),
               Material(
                 elevation: 4,
@@ -153,126 +231,23 @@ void onPhoneNumberChange(String number, String internationalizedPhoneNumber, Str
                     //SHAPE
                     decoration: InputDecoration(
                         contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                        hintText: "Nom",
-
+                        hintText: "Nom d'utilisateur",
                         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
                   ),
                   //Validation de l'entrée 
-                  validator: (val) => val.isEmpty ? 'Entrez votre nom' : null,
+                  validator: (val) => val.isEmpty ? 'Entrez un nom d''utilisateur'  : null,
                   onChanged: (val) {
-                  setState(() => nom = val);
-                },
-              ),
-              ), 
-            /*Champs Email*/ 
-              SizedBox(height: 12.0),
-            /*Champs nom*/ 
-              Material(
-                elevation: 4,
-                 borderRadius: BorderRadius.circular(30.0),
-                child: 
-                TextFormField(
-                  
-                    obscureText: false,
-                    //TEXT 
-                   style: TextStyle(
-                      color:  Colors.grey[900], 
-                      fontFamily: "Roboto",
-                      fontStyle:  FontStyle.normal,
-                      fontSize: 16.0
-                    ), 
-                    //SHAPE
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                        hintText: "Prénom",
-
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-                  ),
-                  //Validation de l'entrée 
-                  validator: (val) => val.isEmpty ? 'Entrez votre prénom' : null,
-                  onChanged: (val) {
-                  setState(() => prenom = val);
-                },
-              ),
-              ), 
-            /*Champs Email*/ 
-            SizedBox(height: 12),
-            /*Champs Nom utilisateur*/ 
-                Material(
-                  
-                elevation: 4,
-                 borderRadius: BorderRadius.circular(30.0),
-                child: 
-                TextFormField(
-                   
-                    obscureText: false,
-                    //TEXT 
-                   style: TextStyle(
-                      color:  Colors.grey[900], 
-                      fontFamily: "Roboto",
-                      fontStyle:  FontStyle.normal,
-                      fontSize: 16.0
-                    ), 
-                    //SHAPE
-                    decoration: InputDecoration(
-                        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                        hintText: "Nom utilisateur",
-
-                        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-                  ),
-                  //Validation de l'entrée 
-                  validator: (val) => val.isEmpty ? 'Entrez votre nom' : null,
-                  onChanged: (val) {
-                  setState(() => utilisateur = val);
+                  setState(() => email = val);
                 },
               ),
               ),
-            /*Champs Nom utilisateur*/ 
-
-              SizedBox(height: 70.0),
-              Material(
-                borderRadius: BorderRadius.circular(30.0),
-                color: Colors.deepOrange,
-                child: 
-                MaterialButton(
-                minWidth: 174,
-                height: 36,
-                child: 
-                Text("SUIVANT",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                      color:  const Color(0xffffffff),
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "Roboto",
-                      fontStyle:  FontStyle.normal,
-                      fontSize: 16.0
-                  ),
-                ),
-                onPressed: () async {
-                  if(_formKey.currentState.validate()){
-                    
-                  }
-                }
-              ),
-              ), 
-            /*  Material(
-              child: InternationalPhoneInput(
-          onPhoneNumberChange: onPhoneNumberChange, 
-          initialPhoneNumber: phoneNumber,
-          initialSelection: phoneIsoCode,
-          enabledCountries: ['+233', '+1']
-       ),), */ 
-              SizedBox(height: 12.0),
-              Text(
-                error,
-                style: TextStyle(color : Colors.red, fontSize: 14.0 ),
-              )
-            ],
-          ),
-        ),
-          
-        ),
-      ),
-    );
+        ],),
+        isActive: _currentStep >=1,  ),
+        Step(
+        title: Text('Etape 3 '),
+        content:TextField(),
+        isActive: _currentStep >=2,  ),
+    ]; 
+    return _steps; 
   }
 }
