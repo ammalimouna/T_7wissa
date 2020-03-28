c28c4d05dc6ad5f9caa856faaeeff038aa45959e
Ammali Mouna <im_ammali@esi.dz>
2020-03-28 17:30:38 +0100

    28-03

---
+++ lib/services/auth.dart
@@ -0,0 +1,65 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myapp/models/user.dart';
+import 'package:myapp/services/database.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User use; 
+
+  // create user obj based on firebase user
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map(_userFromFirebaseUser);
+  }
+
+  
+
+  // sign in with email and password
+  Future signInWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      
+      return user;
+    } catch (error) {
+      print(error.toString());
+      return null;
+    } 
+  }
+
+  // register with email and password
+  Future registerWithEmailAndPassword(String email, String password, String nom, String prenom, String identifiant, String numtel) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      user.sendEmailVerification(); 
+      // create a new document for the user with the uid
+      await DatabaseService(uid: user.uid).updateUserData(nom,prenom, identifiant, numtel);
+      return _userFromFirebaseUser(user);
+    } catch (error) {
+      print(error.toString()); 
+      return null;
+    } 
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (error) {
+      print(error.toString());
+      return null;
+    }
+  }
+  //Mot de passe oublié 
+  Future resetPassword(String email) async {
+    await _auth.sendPasswordResetEmail(email: email);
+}
+}
